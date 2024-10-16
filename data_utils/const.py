b936701acfe4ab079a29e11cc50deadd7a233d64
qiboli <145564757+qibo123@users.noreply.github.com>
2024-10-16 09:52:29 +0800

    Update const.py

--- data_utils/const.py
+++ data_utils/const.py
@@ -1,16 +1,12 @@
-mb1m = 'data/mb1m/output/'
-myket = 'data/myket/output/'
-
+mb1m = 'data/mb1m/'
+myket = 'data/myket/'
 
 coo_record = 'coo_record.npz'
 train_coo_record = 'train_coo_record.npz'
 val_coo_record = 'val_coo_record.npz'
 test_coo_record = 'test_coo_record.npz'
-
 train_skew_coo_record = 'train_skew_coo_record.npz'
-
 timestamps = 'timestamps.npy'
-
 popularity = 'popularity.npy'
 blend_popularity = 'popularity_blend.npy'
 train_coo_adj_graph = 'train_coo_adj_graph.npz'
